851ca82440d72fb30a8a4b02a6ae1b2e0ada414e
TrongHieu90 <tronghieu.designer@gmail.com>
2019-02-01 22:34:05 +0700

    added camera

--- DiabloUnity/Assets/Scripts/CameraFollows.cs
+++ DiabloUnity/Assets/Scripts/CameraFollows.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraFollows : MonoBehaviour
 {
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position;
+
     }
 }
