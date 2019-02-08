0ff95c526419e6bf5141b27c6ee1a907ba737c48
TrongHieu90 <tronghieu.designer@gmail.com>
2019-02-08 09:59:52 +0700

    added new Gizmos distance for visualization.
    added a ShortenDestination method to create a distance to the current click position

--- DiabloUnity/Assets/Scripts/PlayerMovement.cs
+++ DiabloUnity/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    Vector3 currentDestination;
+    Vector3 clickPoint;
 
     [SerializeField] float walkMoveStopRadius = 0.2f;
-        
+    [SerializeField] float attackMoveStopRadius = 5f;
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         m_Character = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;
+        currentDestination = transform.position;
     }
 
     // Fixed update is called in sync with physics
@@ -28,16 +30,18 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+            //print("Cursor raycast hit" + cameraRaycaster.hit.collider.gameObject.name.ToString());
+
+            clickPoint = cameraRaycaster.hit.point;
 
             switch (cameraRaycaster.layerHit)
             {
-                case Layer.Walkable:
-                    currentClickTarget = cameraRaycaster.hit.point;
+                case Layer.Walkable:                   
+                    currentDestination = ShortenDestination(clickPoint, walkMoveStopRadius);
 
                     break;
                 case Layer.Enemy:
-                    print("Not Moving to enemy");
+                    currentDestination = ShortenDestination(clickPoint, attackMoveStopRadius);
                     break;
                 default:
                     print("shouldnt be here");
@@ -46,10 +50,21 @@ public class PlayerMovement : MonoBehaviour
             }
 
         }
+        WalkToDestination();
+        
+    }
 
-        var playerToClickPoint = currentClickTarget - transform.position;
+    private Vector3 ShortenDestination(Vector3 destination, float shortening)
+    {
+        Vector3 reductionVector = (destination - transform.position).normalized * shortening;
+        return destination - reductionVector;
+    }
 
-        if (playerToClickPoint.magnitude >= walkMoveStopRadius)
+    private void WalkToDestination()
+    {
+        var playerToClickPoint = currentDestination - transform.position;
+
+        if (playerToClickPoint.magnitude >= 0)
         {
             m_Character.Move(playerToClickPoint, false, false);
 
@@ -58,6 +73,17 @@ public class PlayerMovement : MonoBehaviour
         {
             m_Character.Move(Vector3.zero, false, false);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(transform.position, currentDestination);
+        Gizmos.DrawSphere(currentDestination, 0.1f);
+        Gizmos.DrawSphere(clickPoint, 0.15f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackMoveStopRadius);
     }
 }
 
